596128fc4d96a9a11b6326f807d52400032be152
valerio-figueira <j.valerio.figueira@gmail.com>
2022-08-16 07:57:09 -0300

    updating code

--- script.js
+++ script.js
@@ -4,34 +4,34 @@ function pullElement(element){
 function pullElements(element){
     return document.querySelectorAll(element);
 }
-function createElement(type, description){
+function setElement(type, description){
     let element = document.createElement(type);
     element.innerHTML = description;
     return element;
 }
 function createSpanInsideElement(type, description, className){
     let element = document.createElement(type);
-    let span = createElement("span", description);
+    let span = setElement("span", description);
     span.className = className;
     element.appendChild(span);
     return element;
 }
 function createList(type, description){
-    let list = createElement("li", description);
+    let list = setElement("li", description);
     type.appendChild(list);
     return type;
 }
 
 let mainBox = pullElement(".main-description");
-mainBox.appendChild(createElement("h1", "Join our community"));
-mainBox.appendChild(createElement("h2", "30-day, hassle-free money back guarantee"));
-mainBox.appendChild(createElement("p", "Gain access to our full library of tutorials along with expert code reviews."));
-mainBox.appendChild(createElement("p", "Perfect for any developers who are serious about honing their skills."));
+mainBox.appendChild(setElement("h1", "Join our community"));
+mainBox.appendChild(setElement("h2", "30-day, hassle-free money back guarantee"));
+mainBox.appendChild(setElement("p", "Gain access to our full library of tutorials along with expert code reviews."));
+mainBox.appendChild(setElement("p", "Perfect for any developers who are serious about honing their skills."));
 
 
 let secondBox = pullElement(".purchase-info");
 
-secondBox.appendChild(createElement("h2", "Monthly Subscription"));
+secondBox.appendChild(setElement("h2", "Monthly Subscription"));
 
 let p = createSpanInsideElement("p", "$29", "price");
 p.innerHTML += "per Month";
@@ -39,17 +39,17 @@ p.className = "center-text";
 secondBox.appendChild(p);
 
 
-secondBox.appendChild(createElement("p", "Full access for less than $1 a day"));
+secondBox.appendChild(setElement("p", "Full access for less than $1 a day"));
 
-let btn = createElement("button", "Sign Up");
+let btn = setElement("button", "Sign Up");
 btn.className = "purchase-btn";
 secondBox.appendChild(btn);
 
 
 let list = pullElement(".list");
-list.appendChild(createElement("h2", "Why Us"));
+list.appendChild(setElement("h2", "Why Us"));
 
-let unorderedList = list.appendChild(createElement("ul", ""));
+let unorderedList = list.appendChild(setElement("ul", ""));
 
 createList(unorderedList, "Tutorials by industry experts");
 createList(unorderedList, "Peer & expert code review");
